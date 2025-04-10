ef47601deac93dfea20ec1d4f06532074f5f0cfa
Gurpreet <ajbagdi6@gmail.com>
2025-04-10 06:18:30 -0500

    updated

--- weather.js
+++ weather.js
@@ -1,77 +1,97 @@
 let btn = document.getElementById("btn");
 let searchInput = document.querySelector("input");
-let cityName = document.querySelector("h2");
+let cityName = document.querySelector("h4");
 let backBackDiv = document.querySelector("#back");
 let imgIcon = document.querySelector("img");
 let temp = document.querySelector("#temp");
 let header = document.querySelector("header");
 let h3 = document.querySelector("h3");
-// imgIcon.src = '/projects images/checked.png'
+let cloudy = document.querySelector("#cloudy");
 
-console.log(searchInput);
-searchInput.addEventListener("input", async function (e) {
-  //   let city = "delhi";
+imgIcon.style.display = "none";
+btn.addEventListener("click", async function () {
   try {
     let key = "b85fb1b7531693be68592c88d144b971";
-    let url = `https://api.openweathermap.org/data/2.5/weather?q=${e.target.value}&appid=${key}&units=metric`;
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${searchInput.value}&appid=${key}&units=metric`;
     let result = await fetch(url);
     let originData = await result.json();
+    let forcast = `https://api.openweathermap.org/data/2.5/forecast?q=${searchInput.value}&appid=${key}&units=metric
+`;
+    let res = await fetch(forcast);
+    let resu = await res.json();
+    console.log(resu);
+    resu.list.forEach((element) => {
+      console.log(element);
+    });
     let apiIcon;
     let srcId;
-    // null
-    if (e.target.value == "") {
-      // alert("please enter city");
-      cityName.innerText = "";
-      imgIcon.src = "";
-      header.classList.remove("cloud");
-      header.classList.remove("rain");
-      header.classList.remove("clear");
-    }
-
+    // if saurce code 200
     if (originData.cod === 200) {
       temp.textContent = originData.main.temp;
+      let date = originData.dt;
+      let realTime = new Date(date * 1000);
+      let weekday = realTime.toLocaleDateString("eng-US", { weekday: "long" });
+      let month = realTime.toLocaleDateString("eng-US", { month: "long" });
+      let day = realTime.toLocaleDateString("eng-US", { day: "numeric" });
+      let temprature = originData.main.temp;
+      let perfectTemp = parseInt(temprature.toFixed(2));
+      imgIcon.style.display = "block";
+
       if (originData.weather[0].main == "Clear") {
         cityName.innerText = originData.name;
-        h3.innerText = "day";
-        // console.log(originData.weather[0].main);
+        h3.innerText = `${weekday}, ${month} ${day}`;
+
         apiIcon = originData.weather[0].icon;
-        console.log(apiIcon);
         srcId = `https://openweathermap.org/img/wn/${apiIcon}@2x.png`;
         imgIcon.src = srcId;
-        header.classList.remove("cloud");
-        header.classList.remove("rain");
-        header.classList.add("clear");
+        cloudy.innerText = originData.weather[0].description;
+        temp.innerText = `${perfectTemp}°`;
+
+        backBackDiv.classList.remove("cloud");
+        backBackDiv.classList.remove("rain");
+        backBackDiv.classList.add("clear");
       } else if (originData.weather[0].main == "Clouds") {
-        h3.innerText = "night";
+        cityName.innerText = originData.name;
+        h3.innerText = `${weekday}, ${month} ${day}`;
+
         apiIcon = originData.weather[0].icon;
-        console.log(apiIcon);
         srcId = `https://openweathermap.org/img/wn/${apiIcon}@2x.png`;
         imgIcon.src = srcId;
-        header.classList.remove("clear");
-        header.classList.remove("rain");
-        header.classList.add("cloud");
+
+        cloudy.innerText = originData.weather[0].main;
+        temp.innerText = `${perfectTemp}°`;
+        console.log(perfectTemp);
+        backBackDiv.classList.remove("clear");
+        backBackDiv.classList.remove("rain");
+        backBackDiv.classList.add("cloud");
         console.log(originData.weather[0].main);
       } else if (originData.weather[0].main == "Rain") {
+        cityName.innerText = originData.name;
+        h3.innerText = `${weekday}, ${month} ${day}`;
+
         apiIcon = originData.weather[0].icon;
-        console.log(apiIcon);
         srcId = `https://openweathermap.org/img/wn/${apiIcon}@2x.png`;
         imgIcon.src = srcId;
-        console.log("ranny");
-        header.classList.remove("clear");
-        header.classList.remove("cloud");
-        header.classList.add("rain");
+
+        temp.innerText = `${perfectTemp}°`;
+        cloudy.innerText = originData.weather[0].main;
+        backBackDiv.classList.remove("clear");
+        backBackDiv.classList.remove("cloud");
+        backBackDiv.classList.add("rain");
       }
-
       console.log(originData);
+    } else {
+      if (searchInput.value == "") {
+        alert("please enter city");
+        cityName.innerText = "";
+        document.querySelector("img").remove();
+        backBackDiv.classList.remove("cloud");
+        backBackDiv.classList.remove("rain");
+        backBackDiv.classList.remove("clear");
+        console.log("search empty");
+      }
     }
-
-    // if (e.target.value == "delhi") {
-    //   header.classList.add();
-
-    //   console.log("correct");
-    // } else if (e.target.value == "") {
-    //   console.log("please enter a name");
-    // }
+    searchInput.value = "";
   } catch (error) {
     console.log("data cannot finded", error);
   }
